ac9be049ce7cb5b87647fa968af679a7d4af5c48
XiNGRZ <chenxingyu92@gmail.com>
2019-11-09 12:48:37 +0800

    icesky: Build libbt-vendor from source
    Change-Id: Idd11125d26062c416b61600e654510bca7c82c97

--- icesky-vendor.mk
+++ icesky-vendor.mk
@@ -116,7 +116,6 @@ PRODUCT_COPY_FILES += \
     vendor/smartisan/icesky/proprietary/vendor/lib/libadiertac.so:$(TARGET_COPY_OUT_VENDOR)/lib/libadiertac.so \
     vendor/smartisan/icesky/proprietary/vendor/lib/libadsprpc.so:$(TARGET_COPY_OUT_VENDOR)/lib/libadsprpc.so \
     vendor/smartisan/icesky/proprietary/vendor/lib/libaudcal.so:$(TARGET_COPY_OUT_VENDOR)/lib/libaudcal.so \
-    vendor/smartisan/icesky/proprietary/vendor/lib/libbt-vendor.so:$(TARGET_COPY_OUT_VENDOR)/lib/libbt-vendor.so \
     vendor/smartisan/icesky/proprietary/vendor/lib/libchromatix_liteon_imx214_common.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_liteon_imx214_common.so \
     vendor/smartisan/icesky/proprietary/vendor/lib/libchromatix_liteon_imx214_cpp_hfr_120.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_liteon_imx214_cpp_hfr_120.so \
     vendor/smartisan/icesky/proprietary/vendor/lib/libchromatix_liteon_imx214_cpp_hfr_60.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_liteon_imx214_cpp_hfr_60.so \
@@ -322,7 +321,6 @@ PRODUCT_COPY_FILES += \
     vendor/smartisan/icesky/proprietary/vendor/lib64/libadiertac.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libadiertac.so \
     vendor/smartisan/icesky/proprietary/vendor/lib64/libadsprpc.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libadsprpc.so \
     vendor/smartisan/icesky/proprietary/vendor/lib64/libaudcal.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libaudcal.so \
-    vendor/smartisan/icesky/proprietary/vendor/lib64/libbt-vendor.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libbt-vendor.so \
     vendor/smartisan/icesky/proprietary/vendor/lib64/libhwdaphal.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhwdaphal.so \
     vendor/smartisan/icesky/proprietary/vendor/lib64/libsensor1.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsensor1.so \
     vendor/smartisan/icesky/proprietary/vendor/lib64/libsensor_reg.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsensor_reg.so
